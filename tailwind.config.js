3b665d674549fd91b9a374542a244581142900b2
budayyy <budibuday97@gmail.com>
2022-07-12 14:08:59 +0700

    update portfolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0284c7',
+        dark: '#1e293b',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
